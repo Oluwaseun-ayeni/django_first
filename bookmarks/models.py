8e684affde8b7a67dc1961fe7beaab8a4eed267c
Oluwaseun Isaac <72kulbranking@gmail.com>
2022-08-28 16:57:16 +0100

    first commit

---
+++ bookmarks/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Link(models.Model):
+    
+    url = models.URLField(unique=True)
+
+class Users(models.Model):
+    username = models.CharField( max_length=40)
+    password = models.CharField( max_length=200) 
+    email = models.EmailField(max_length=75)
+
+class Bookmark(models.Model):
+    title = models.CharField(max_length=200)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    link = models.ForeignKey(Link, on_delete=models.CASCADE)   
+
+
+# Create your models here.
